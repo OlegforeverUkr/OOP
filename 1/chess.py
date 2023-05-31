8a1ef3057ddffb8259bcc3b70a9e7c39f8d4cd03
Oleh Shliapin <shlyapin8848@gmail.com>
2023-05-31 20:42:41 +0300

    OOP

--- 1/chess.py
+++ 1/chess.py
@@ -14,14 +14,16 @@ class Chess:
         else:
             print("Invalid position. Position coordinates should be between 0 and 7.")
 
-    def check_move(self, new_position: tuple) -> bool:  # checks the possibility of a move
-        self.change_position(new_position)
-        return True
+    def check_move(self, new_position: tuple) -> bool:   # checking the ability to make a move
+        if 0 <= new_position[0] <= 7 and 0 <= new_position[1] <= 7:
+            return True
+        else:
+            return False
 
 
 class Pawn(Chess):
 
-    def move(self, new_position: tuple):
+    def move(self, new_position: tuple): # move method for Pawn
         self.old_position = self.position
         x, y = self.position
         new_x, new_y = new_position
@@ -43,11 +45,74 @@ class Pawn(Chess):
                 self.position = self.old_position
 
 
-t1 = Pawn()
-t1.position = (3, 2)
-t1.move((3, 9))
-# t2 = Pawn()
-# t2.color = 'black'
-# t2.position = (3, 6)
-# t2.move((3, 5))
+class King(Chess):
+
+    def move(self, new_position: tuple):  # move method for King
+        self.old_position = self.position
+        x, y = self.position
+        new_x, new_y = new_position
+
+        if self.check_move(new_position):
+            if -1 >= new_x - x <= 1 and -1 >= new_y - y <= 1:
+                self.position = (new_x, new_y)
+                print('You make a move')
+            else:
+                print("Can't make this move")
+                self.position = self.old_position
+
+
+class Rook(Chess):
+
+    def move(self, new_position: tuple):
+        self.old_position = self.position
+        x, y = self.position
+        new_x, new_y = new_position
+
+        if self.check_move(new_position):
+            if (x == new_x and y != new_y) or (x != new_x and y == new_y):
+                self.position = (new_x, new_y)
+                print('You make a move')
+            else:
+                print("Can't make this move")
+                self.position = self.old_position
+
+
+class Bishop(Chess):
+
+    def move(self, new_position: tuple):
+        self.old_position = self.position
+        x, y = self.position
+        new_x, new_y = new_position
+
+        if self.check_move(new_position):
+            if new_x - x == new_y - y:
+                self.position = (new_x, new_y)
+                print('You make a move')
+            else:
+                print("Can't make this move")
+                self.position = self.old_position
+
+
+class Queen(Chess):
+
+    def move(self, new_position: tuple):
+        self.old_position = self.position
+        x, y = self.position
+        new_x, new_y = new_position
+
+        if self.check_move(new_position):
+            if (x == new_x and y != new_y) or (x != new_x and y == new_y) or (new_x - x == new_y - y):
+                self.position = (new_x, new_y)
+                print('You make a move')
+            else:
+                print("Can't make this move")
+                self.position = self.old_position
+        else:
+            print("Can't make this move")
+            self.position = self.old_position
+
+
+t1 = Bishop()
+t1.position = (4, 0)
+t1.move((5, 1))
 print(t1.position)
